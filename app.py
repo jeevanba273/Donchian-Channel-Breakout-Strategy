6226ac33fc2155aac11a3af28372e84d6bdebac4
JEEVAN B A <87776020+jeevanba273@users.noreply.github.com>
2025-03-09 14:22:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -532,12 +532,12 @@ def comprehensive_stock_analysis(ticker_symbol, file_path,
 st.set_page_config(page_title="Donchian Channel Breakout: Detailed Drawdown & Advanced Indicator Suite", layout="wide")
 st.title("Donchian Channel Breakout: Detailed Drawdown & Advanced Indicator Suite")
 
-st.sidebar.write("`Created by:`")
+st.sidebar.write("Created by:")
 linkedin_url = "https://www.linkedin.com/in/jeevanba273/"
 st.sidebar.markdown(
     f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: black;">'
-    f'<img src="assets/linkedin_icon.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">'
-    f'`JEEVAN B A`</a>',
+    f'<img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">'
+    f'JEEVAN B A</a>',
     unsafe_allow_html=True
 )
 
